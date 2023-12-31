4e8ffcf8c14ae3a0ba91a015494847c2ded2554d
berry <100073317+berryywise@users.noreply.github.com>
2023-12-31 16:24:01 +0100

    Added logic to add books and basic UI

--- sript.js
+++ sript.js
@@ -1,3 +1,8 @@
+let myLibrary = [];
+let container = document.querySelector(".container")
+let confirmbutton = document.querySelector("#confirm-button")
+
+
 function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -9,5 +14,48 @@ function Book (title, author, pages, read) {
 }
 
 
-const book1 = new Book("Zippie", "Berry", "322", "read")
-console.log(book1.info())
+const addToLibrary = () => {
+
+    let authorinput = document.querySelector("#author").value
+    let titleinput = document.querySelector("#title").value
+    let pagesinput = document.querySelector("#pages").value
+    let readinput = document.querySelector("#read").value
+
+    let obj = new Book(titleinput, authorinput, pagesinput, readinput);
+
+    myLibrary.push(obj);
+
+    console.log(myLibrary);
+}
+
+const refreshUI = array => {
+
+    
+    array.forEach(element => {
+
+        let para = document.createElement("p");
+
+        para.classList.add("books")
+
+        para.innerText = element;
+
+        container.appendChild(para);
+
+    });
+
+
+}
+
+confirmbutton.addEventListener("click", event => {
+
+    addToLibrary();
+
+
+})
+
+
+refreshUI(["test", "test", "test","test", "test", "test","test", "test", "test"]);
+
+
+// const book1 = new Book("Zippie", "Berry", "322", "read")
+// console.log(book1.info())
