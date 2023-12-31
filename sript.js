3f6082d907d183f2adbb9e61b93428e9155b9827
pc <100073317+berryywise@users.noreply.github.com>
2023-12-31 13:33:08 +0100

    Boilerplate object constructors

---
+++ sript.js
@@ -0,0 +1,13 @@
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+
+const book1 = new Book("Zippie", "Berry", "322", "read")
+console.log(book1.info())
